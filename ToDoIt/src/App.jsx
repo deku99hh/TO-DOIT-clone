c520aaec57636ac2a9c9cdfa96ca4001bd0b0268
deku99hh <omar.rashed1000@gmail.com>
2026-02-09 04:34:03 +0200

    finaly something is on screen

--- ToDoIt/src/App.jsx
+++ ToDoIt/src/App.jsx
@@ -1,35 +1,151 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
+import { MyCridet } from './comp/MyCridet'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MyCridet />
+      <main>
+
+        <div className="topMain">
+          <h1>Todo List</h1>
+          <div className="fourBlockes">
+            <div className="block1 block">
+              <p className="subTitle">Total Tasks</p>
+              <div className="nomberAndIcon">
+                <p>1</p>
+                <i className="bx bx-task text-2xl"></i>
+              </div>
+            </div>
+
+            <div className="block2 block">
+              <p className="subTitle">Total Tasks</p>
+              <div className="nomberAndIcon">
+                <p>0</p>
+                <i className="bx bx-check-circle text-2xl"></i>
+              </div>
+            </div>
+
+            <div className="block3 block">
+              <p className="subTitle">Total Tasks</p>
+              <div className="nomberAndIcon">
+                <p>1</p>
+                <i className="bx bx-time text-2xl"></i>
+              </div>
+            </div>
+
+            <div className="block4 block">
+              <p className="subTitle">Total Tasks</p>
+              <div className="nomberAndIcon">
+                <p>0%</p>
+                <i className="bx bx-pie-chart-alt text-2xl"></i>
+              </div>
+            </div>
+          </div>
+          <div className="loodingBar">
+          </div>
+        </div>
+
+        <div className="bottomMain">
+          <div className="interractian">
+            <input type="text" className="text" placeholder="add a todo..." />
+            <input type="date" className="date" />
+            <button>
+              <i className="bx bx-plus bx-sm"></i>
+            </button>
+
+          </div>
+
+
+          <div className="sershAndSort">
+            <input type="text" placeholder="search tasks..." />
+            <button><i className="bx bx-filter mr-2"></i>filter</button>
+          </div>
+          <div className="towButtons">
+            <button className="SORT"><i className="bx bx-sort mr-1"></i>  SORT</button>
+            <button className="deleteAll"><i className="bx bx-trash mr-2"></i>  Delete All</button>
+          </div>
+
+
+          <table className="table table-zebra w-full">
+            <thead>
+              <tr>
+                <th className="w-2/5">Task</th>
+                <th className="w-1/5">Due Date</th>
+                <th className="w-1/5">Status</th>
+                <th className="w-1/5">Actions</th>
+              </tr>
+            </thead>
+            <tbody className="todos-list-body"><tr className="todo-item  " data-id="mwvfpf848yo44sgi7fc6bt" draggable="true">
+              <td className="">
+                <div className="flex items-center">
+
+                  <span className="" data-original="sfdfZDF">
+                    sfdfZDF
+                  </span>
+
+                </div>
+              </td>
+              <td>No due date</td>
+              <td>
+                <div className="badge badge-warning">
+                  Pending
+                </div>
+              </td>
+              <td>
+                <div className="flex gap-1">
+
+                  <button className="btn btn-warning btn-xs edit-btn" data-id="mwvfpf848yo44sgi7fc6bt" title="Edit">
+                    <i className="bx bx-edit-alt"></i>
+                  </button>
+                  <button className="btn btn-success btn-xs toggle-btn" data-id="mwvfpf848yo44sgi7fc6bt" title="Mark Complete">
+                    <i className="bx bx-check"></i>
+                  </button>
+                  <button className="btn btn-error btn-xs delete-btn" data-id="mwvfpf848yo44sgi7fc6bt" title="Delete">
+                    <i className="bx bx-trash"></i>
+                  </button>
+                </div>
+              </td>
+            </tr><tr className="todo-item  " data-id="4k57gjzfy8o3kt0phlwjv3" draggable="true">
+                <td className="">
+                  <div className="flex items-center">
+
+                    <span className="" data-original="fdsfsdf">
+                      fdsfsdf
+                    </span>
+
+                  </div>
+                </td>
+                <td>No due date</td>
+                <td>
+                  <div className="badge badge-warning">
+                    Pending
+                  </div>
+                </td>
+                <td>
+                  <div className="flex gap-1">
+
+                    <button className="btn btn-warning btn-xs edit-btn" data-id="4k57gjzfy8o3kt0phlwjv3" title="Edit">
+                      <i className="bx bx-edit-alt"></i>
+                    </button>
+                    <button className="btn btn-success btn-xs toggle-btn" data-id="4k57gjzfy8o3kt0phlwjv3" title="Mark Complete">
+                      <i className="bx bx-check"></i>
+                    </button>
+                    <button className="btn btn-error btn-xs delete-btn" data-id="4k57gjzfy8o3kt0phlwjv3" title="Delete">
+                      <i className="bx bx-trash"></i>
+                    </button>
+                  </div>
+                </td>
+              </tr></tbody>
+          </table>
+
+        </div>
+
+      </main>
     </>
   )
 }
-
-export default App
---
+++ ToDoIt/src/comp/MyCridet.jsx
@@ -0,0 +1,10 @@
+import './MyCridet.css'
+
+export function MyCridet(){
+
+    return (
+        <div className="cridet">
+            made by deku99hh
+        </div>
+    );
+}
